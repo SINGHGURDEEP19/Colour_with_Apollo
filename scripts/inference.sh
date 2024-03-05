ba40094575dddf6ea657a6eaf5818637c2d6b24f
GURDEEP SINGH <127899720+SINGHGURDEEP19@users.noreply.github.com>
2024-03-05 14:05:52 +0530

    Update inference.sh

--- scripts/inference.sh
+++ scripts/inference.sh
@@ -1,4 +1,4 @@
 CUDA_VISIBLE_DEVICES=0 \
 python3 inference/colorization_pipeline.py \
 	--input ./assets/test_images --output ./colorize_output \
-	--model_path modelscope/damo/cv_ddcolor_image-colorization/pytorch_model.pt
+	--model_path modelscope/damo/cv_colour_with_apollo_image-colorization/pytorch_model.pt
